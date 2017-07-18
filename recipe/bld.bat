b243c72fa546fb20a4cceab23653ddafd92868f1
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-18 10:49:28 -0300

    stdbool

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,12 @@
+set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
+set INCLUDE=%LIBRARY_INC%;%INCLUDE%
+
+:: VS2008 doesn't have stdbool.h so copy in our own
+:: to 'lib' where the other headers are so it gets picked up.
+if "%VS_MAJOR%" == "9" (
+    copy %RECIPE_DIR%\stdbool.h src\
+)
+
 mkdir build
 cd build
 
