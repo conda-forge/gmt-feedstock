5fe453a754a902ea238e5372ebea0a6211ce91f5
Leonardo Uieda <leouieda@gmail.com>
2017-07-20 15:18:04 -1000

    Use DCW and GSHHG packaged data

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
 GSHHG_DIR="$PREFIX/share/gshhg-gmt"
+DCW_DIR="$PREFIX/share/dcw-gmt"
 
-# TODO: Maybe split this into another package.
-DATADIR="$PREFIX/share/coast"
-mkdir -p $DATADIR
-
-# DCW (country polygons):
-DCW="dcw-gmt-1.1.2"
-URL="ftp://ftp.soest.hawaii.edu/dcw/$DCW.$EXT"
-curl $URL > $DCW.$EXT
-tar xzf $DCW.$EXT
-cp $DCW/* $DATADIR
-
-#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export LDFLAGS=
 
 mkdir build && cd build
@@ -25,7 +14,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D PCRE_ROOT=$PREFIX \
       -D ZLIB_ROOT=$PREFIX \
       -D GMT_LIBDIR=$PREFIX/lib \
-      -D DCW_ROOT=$DATADIR \
+      -D DCW_ROOT=$DCW_DIR \
       -D GSHHG_ROOT=$GSHHG_DIR \
       -D GMT_INSTALL_TRADITIONAL_FOLDERNAMES:BOOL=FALSE \
       -D GMT_INSTALL_MODULE_LINKS:BOOL=FALSE \
@@ -35,21 +24,16 @@ make -j$CPU_COUNT
 make check
 make install
 
-#we are fixing the paths to dynamic library files inside library and
-#binary files because something in make install is doubling up the
-#path to the library files.  Anyone who knows how to solve that
-#problem is free to contact the maintainers.
-
+# We are fixing the paths to dynamic library files inside library and binary
+# files because something in 'make install' is doubling up the path to the
+# library files. This only happens on OSX. Anyone who knows how to solve that
+# problem is free to contact the maintainers.
 if [[ "$(uname)" == "Darwin" ]];then
     install_name_tool -id $PREFIX/lib/libgmt.5.dylib $PREFIX/lib/libgmt.5.dylib
     install_name_tool -id $PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libpostscriptlight.5.dylib
-
     install_name_tool -change $PREFIX/$PREFIX/lib/libgmt.5.dylib $PREFIX/lib/libgmt.5.dylib $PREFIX/lib/gmt/plugins/supplements.so
     install_name_tool -change $PREFIX/$PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/gmt/plugins/supplements.so
-
     install_name_tool -change $PREFIX/$PREFIX/lib/libgmt.5.dylib $PREFIX/lib/libgmt.5.dylib $PREFIX/bin/gmt
     install_name_tool -change $PREFIX/$PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/bin/gmt
-    
-    install_name_tool -change $PREFIX/$PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libgmt.5.4.2.dylib
+    install_name_tool -change $PREFIX/$PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libpostscriptlight.5.dylib $PREFIX/lib/libgmt.5.dylib
 fi
-
